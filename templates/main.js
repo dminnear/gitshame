d196fc4ff64e7f87c18a8cabeafe0be4591f46a6
Drew Minnear <drew.minnear@originate.com>
2016-03-28 16:30:53 -0400

    add request to api gateway

--- templates/main.js
+++ templates/main.js
@@ -5,11 +5,22 @@ function openModal() {
 
 function closeModalEvent(event) {
   if (event.target.id == "modal") {
-    closeModal()
+    closeModal();
   }
 }
 
 function closeModal() {
   el = document.getElementById("modal");
   el.style.visibility = "hidden";
+}
+
+function shame() {
+  link = document.getElementById("link").value;
+  httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = function(){
+    location.reload()
+  };
+  httpRequest.open('GET', 'https://5w7zwh5alf.execute-api.us-east-1.amazonaws.com/prod/pygmentize?link=' + link, true);
+  httpRequest.send(null);
+  closeModal();
 }
