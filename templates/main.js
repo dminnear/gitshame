d1c93e42270a576ecef522c7d4efe978391eb19c
Drew Minnear <drew.minnear@originate.com>
2016-03-28 17:10:55 -0400

    fixed border

--- templates/main.js
+++ templates/main.js
@@ -17,9 +17,6 @@ function closeModal() {
 function shame() {
   link = document.getElementById("link").value;
   httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = function(){
-    location.reload()
-  };
   httpRequest.open('GET', 'https://5w7zwh5alf.execute-api.us-east-1.amazonaws.com/prod/pygmentize?link=' + link, true);
   httpRequest.send(null);
   closeModal();
